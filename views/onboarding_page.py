6fa109e3387523d4585365c2534d9478fa9a588b
treppenwitz03 <owenlegrande@gmail.com>
2023-11-22 11:33:15 +0800

    Add Onboarding View

--- views/onboarding_page.py
+++ views/onboarding_page.py
@@ -1,26 +1,4 @@
 import flet as ft
-
-def main(page: ft.Page):
-
-    c1 = ft.Container(width=50, height=50, bgcolor="red", animate_position=200)
-
-    def animate_container(e):
-        c1.left = page.width - c1.width - 20 # padding of left and right
-        page.update()
-    
-    def follow_resize(e):
-        print("RESIZING")
-    
-    page.on_resize = animate_container
-
-    page.add(
-        ft.Stack([c1], height=250, expand=True),
-        ft.ElevatedButton("Animate!", on_click=animate_container),
-    )
-
-ft.app(target=main)
-
-import flet as ft
 from flet_route import Params, Basket
 
 class OnboardingPage():
@@ -44,7 +22,7 @@ class OnboardingPage():
             horizontal_alignment=ft.CrossAxisAlignment.START
         )
         
-        main_column = ft.Column(
+        self.main_column = ft.Column(
             controls = [
                 logo_row,
                 ft.Text("Welcome to Morax", weight=ft.FontWeight.BOLD, size=44),
@@ -69,13 +47,13 @@ class OnboardingPage():
             controls=[gcash_logo]
         )
         
-        qr_image = ft.Image(
+        self.qr_image = ft.Image(
             "resources/sample_qr.png",
             width=100,
             height=100
         )
         
-        qr_upload_button = ft.ElevatedButton(
+        self.qr_upload_button = ft.ElevatedButton(
             bgcolor = "#d6d6d6",
             width = 200,
             height = 44,
@@ -87,7 +65,7 @@ class OnboardingPage():
         )
         
         qr_column = ft.Column(
-            controls=[qr_image, qr_upload_button],
+            controls=[self.qr_image, self.qr_upload_button],
             horizontal_alignment=ft.CrossAxisAlignment.CENTER,
             spacing=20
         )
@@ -97,7 +75,7 @@ class OnboardingPage():
             weight=ft.FontWeight.W_600
         )
         
-        number_textfield = ft.TextField(
+        self.number_textfield = ft.TextField(
             label="Enter your number here",
             color = ft.colors.BLACK,
             border_radius = 25,
@@ -111,7 +89,7 @@ class OnboardingPage():
         )
         
         number_column = ft.Column(
-            controls=[number_label, number_textfield],
+            controls=[number_label, self.number_textfield],
             horizontal_alignment=ft.CrossAxisAlignment.CENTER,
             spacing=20
         )
@@ -122,11 +100,11 @@ class OnboardingPage():
             spacing=50
         )
         
-        gcash_column = ft.Column(
+        self.gcash_column = ft.Column(
             controls = [
                 gcash_logo_row,
-                ft.Text("Update your GCash Credentials", weight=ft.FontWeight.BOLD, size=44),
-                ft.Text("GCash is the sole supported payment platform of Morax.", weight=ft.FontWeight.W_400, size=20, color="#4d4d4d"),
+                ft.Text("Update your Profile Picture", weight=ft.FontWeight.BOLD, size=44),
+                ft.Text("Profile pictures allow you to be easily", weight=ft.FontWeight.W_400, size=20, color="#4d4d4d"),
                 ft.Container(options_row, padding=ft.padding.only(30, 100, 30, 100))
             ],
             horizontal_alignment=ft.CrossAxisAlignment.CENTER,
@@ -147,16 +125,16 @@ class OnboardingPage():
             controls=[profile_logo]
         )
         
-        user_image = ft.Image(
+        self.user_image = ft.Image(
             "resources/empty_user_image.svg",
-            width=200,
-            height=200
+            width=100,
+            height=100
         )
         
-        profile_upload_button = ft.ElevatedButton(
+        self.profile_upload_button = ft.ElevatedButton(
             bgcolor = "#d6d6d6",
-            width = 200,
-            height = 44,
+            width = 150,
+            height = 32,
             content=ft.Text(
                 value="Upload Photo",
                 color = "#ae8948",
@@ -165,11 +143,11 @@ class OnboardingPage():
         )
         
         profile_upload_column = ft.Column(
-            controls=[user_image, profile_upload_button],
-            horizontal_alignment=ft.CrossAxisAlignment.START
+            controls=[self.user_image, self.profile_upload_button],
+            horizontal_alignment=ft.CrossAxisAlignment.CENTER
         )
         
-        profile_column = ft.Column(
+        self.profile_column = ft.Column(
             controls = [
                 profile_logo_row,
                 ft.Text("Update your Profile Picture", weight=ft.FontWeight.BOLD, size=44),
@@ -184,10 +162,10 @@ class OnboardingPage():
         )
         
         switcher = ft.Stack(
-            controls=[main_column, gcash_column, profile_column]
+            controls=[self.main_column, self.gcash_column, self.profile_column]
         )
         
-        next_button = ft.ElevatedButton(
+        self.next_button = ft.ElevatedButton(
             "Next",
             width=150,
             bgcolor="#ae8948",
@@ -195,8 +173,9 @@ class OnboardingPage():
         )
         
         navigation_row = ft.Row(
-            controls=[next_button],
-            alignment=ft.MainAxisAlignment.SPACE_BETWEEN
+            controls=[self.next_button],
+            alignment=ft.MainAxisAlignment.END,
+            vertical_alignment=ft.CrossAxisAlignment.END
         )
         
         self.view = ft.View(
@@ -206,28 +185,6 @@ class OnboardingPage():
             vertical_alignment=ft.MainAxisAlignment.SPACE_BETWEEN,
             padding=30
         )
-        
-        current = 0
-        
-        def click(event: ft.ControlEvent):
-            if current == 0:
-                main_column.offset = ft.transform.Offset(-1, 0)
-                main_column.update()
-                gcash_column.offset = ft.transform.Offset(0, 0)
-                gcash_column.update()
-                profile_column.offset = ft.transform.Offset(1, 0)
-                profile_column.update()
-                current = 1
-            elif current == 1:
-                main_column.offset = ft.transform.Offset(-2, 0)
-                main_column.update()
-                gcash_column.offset = ft.transform.Offset(-1, 0)
-                gcash_column.update()
-                profile_column.offset = ft.transform.Offset(0, 0)
-                profile_column.update()
-                current = 2
-        
-        next_button.on_click = click
     
     def get_view(self, page: ft.Page, params: Params, basket: Basket):
         self.basket = basket
